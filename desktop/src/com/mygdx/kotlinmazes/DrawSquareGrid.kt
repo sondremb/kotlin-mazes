5348562de65d8d57b22d4774c19db66860a7ccf2
sondremb <sondremartin@hotmail.com>
2023-02-08 20:05:48 +0100

    Skriv om DrawSquareGrid til ktx.graphics.use

--- desktop/src/com/mygdx/kotlinmazes/DrawSquareGrid.kt
+++ desktop/src/com/mygdx/kotlinmazes/DrawSquareGrid.kt
@@ -8,6 +8,7 @@ import com.mygdx.kotlinmazes.graphics.Gradient
 import com.mygdx.kotlinmazes.grids.square.SquareGrid
 import com.mygdx.kotlinmazes.math.norm
 import com.mygdx.kotlinmazes.math.vec2Int
+import ktx.graphics.use
 
 
 class DrawSquareGrid(private val grid: SquareGrid, private val distance: Distance) : Scene() {
@@ -15,11 +16,11 @@ class DrawSquareGrid(private val grid: SquareGrid, private val distance: Distanc
     // farger basert på colormapet "plasma" fra matplotlib
     // hex-koder hentet fra https://waldyrious.net/viridis-palette-generator/
     private var gradient: Gradient = Gradient(
-            Color.valueOf("#f0f921"),
-            Color.valueOf("#f89540"),
-            Color.valueOf("#cc4778"),
-            Color.valueOf("#7e03a8"),
-            Color.valueOf("#0d0887")
+        Color.valueOf("#f0f921"),
+        Color.valueOf("#f89540"),
+        Color.valueOf("#cc4778"),
+        Color.valueOf("#7e03a8"),
+        Color.valueOf("#0d0887")
     )
 
     override fun init() {
@@ -29,31 +30,31 @@ class DrawSquareGrid(private val grid: SquareGrid, private val distance: Distanc
 
     override fun render() {
         ScreenUtils.clear(1f, 1f, 1f, 1f)
-        for (cell in grid.cells) {
-            val r = cell.row
-            val c = cell.column
+        shape.use(ShapeRenderer.ShapeType.Filled) {
+            for (cell in grid.cells) {
+                val r = cell.row
+                val c = cell.column
 
-            shape.begin(ShapeRenderer.ShapeType.Filled)
-            val color = gradient.sample(norm(0f, distance.max.toFloat(), distance[cell].toFloat()))
-            shape.color = color
-            shape.rect(c.toFloat(), r.toFloat(), 1f, 1f)
-            shape.end()
+                it.color = gradient.sample(norm(0f, distance.max.toFloat(), distance[cell].toFloat()))
+                it.rect(c.toFloat(), r.toFloat(), 1f, 1f)
+            }
+        }
 
-            shape.begin(ShapeRenderer.ShapeType.Line)
-            shape.color = Color.BLACK
-            if (!cell.isLinked(cell.north)) {
-                shape.line(vec2Int(c, r + 1), vec2Int(c + 1, r + 1))
+        shape.use(ShapeRenderer.ShapeType.Line) {
+            it.color = Color.BLACK
+            it.line(vec2Int(0, 0), vec2Int(0, grid.height))
+            it.line(vec2Int(0, 0), vec2Int(grid.width, 0))
+
+            for (cell in grid.cells) {
+                val r = cell.row
+                val c = cell.column
+                if (!cell.isLinked(cell.north)) {
+                    it.line(vec2Int(c, r + 1), vec2Int(c + 1, r + 1))
+                }
+                if (!cell.isLinked(cell.east)) {
+                    it.line(vec2Int(c + 1, r), vec2Int(c + 1, r + 1))
+                }
             }
-            if (!cell.isLinked(cell.east)) {
-                shape.line(vec2Int(c + 1, r), vec2Int(c + 1, r + 1))
-            }
-            if (!cell.isLinked(cell.south)) {
-                shape.line(vec2Int(c, r), vec2Int(c + 1, r))
-            }
-            if (!cell.isLinked(cell.west)) {
-                shape.line(vec2Int(c, r), vec2Int(c, r + 1))
-            }
-            shape.end()
         }
     }
 }
--- desktop/src/com/mygdx/kotlinmazes/main.kt
+++ desktop/src/com/mygdx/kotlinmazes/main.kt
@@ -2,6 +2,7 @@ package com.mygdx.kotlinmazes
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.mygdx.kotlinmazes.generation.aldousBroder
 import com.mygdx.kotlinmazes.generation.sideWinder
 import com.mygdx.kotlinmazes.grids.square.SquareGrid
 
@@ -10,7 +11,7 @@ fun main(arg: Array<String>) {
     val config = Lwjgl3ApplicationConfiguration()
     config.setForegroundFPS(60)
     config.setTitle("Kotlin Mazes")
-    val grid = SquareGrid(50, 80).also { sideWinder(it) }
-    val dists = Distance(grid.get(0, 0)!!)
+    val grid = SquareGrid(50, 80).also { aldousBroder(it) }
+    val dists = Distance(grid.get(grid.height / 2, grid.width / 2)!!)
     Lwjgl3Application(DrawSquareGrid(grid, dists), config)
 }
