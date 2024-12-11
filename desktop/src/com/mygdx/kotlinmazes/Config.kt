6523cbc4487a96d1f15eba4474802107cad1e6ae
sondremb <sondremartin@hotmail.com>
2024-12-12 00:21:34 +0100

    Forenkle MaskedSquareGrid

--- desktop/src/com/mygdx/kotlinmazes/Config.kt
+++ desktop/src/com/mygdx/kotlinmazes/Config.kt
@@ -4,8 +4,8 @@ import com.badlogic.gdx.math.Rectangle
 import ktx.math.vec2
 
 object Config {
-    const val VIEWPORT_WIDTH = 512f
-    const val VIEWPORT_HEIGHT = 512f
+    const val VIEWPORT_WIDTH = 1920f
+    const val VIEWPORT_HEIGHT = 1080f
     val VIEWPORT_CENTER = vec2(VIEWPORT_WIDTH / 2, VIEWPORT_HEIGHT / 2)
     val VIEWPORT_RECT = Rectangle(0f, 0f, VIEWPORT_WIDTH, VIEWPORT_HEIGHT)
 }
--- desktop/src/com/mygdx/kotlinmazes/scenes/MaskedSquareGrid.kt
+++ desktop/src/com/mygdx/kotlinmazes/scenes/MaskedSquareGrid.kt
@@ -4,50 +4,43 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.utils.ScreenUtils
-import com.mygdx.kotlinmazes.solving.Distance
 import com.mygdx.kotlinmazes.drawers.SquareGridDrawer
 import com.mygdx.kotlinmazes.grids.square.MaskedSquareGrid
 import com.mygdx.kotlinmazes.grids.square.SquareCell
 import com.mygdx.kotlinmazes.mazegeneration.AldousBroder
-import com.mygdx.kotlinmazes.utils.graphics.Gradient
 
 fun main() {
     val grid = MaskedSquareGrid(108, 192)
     MaskedSquareGridScene(grid).play()
 }
 
+// [VENSTREKLIKK] u-maskerer cellen så den kan bli en del av labyrinten
+// [HØYREKLIKK] maskerer cellen så den ikke kan bli en del av labyrinten
+// [SPACE] genererer en labyrint
+// [PIL OPP] øk størrelsen på "penselen"
+// [PIL NED] reduser størrelsen på "penselen"
 class MaskedSquareGridScene(private val grid: MaskedSquareGrid) : Scene() {
 
-    private var distanceSource: SquareCell? = null
-    private var distance: Distance? = null
     private lateinit var drawer: SquareGridDrawer
-    private val gradient = Gradient.Plasma
     private var prevHoveredCell: SquareCell? = null
     private var isLeftHeld = false
     private var isRightHeld = false
-    private var isForwardHeld = false
-    private var isBackHeld = false
-    private var isSpaceHeld = false
     private var range = 2
-    private var currentDistance = 0
+    private var hasLabyrinth = false
 
     override fun init() {
         drawer = SquareGridDrawer(shapeRenderer, grid)
-        //grid.allCells().forEach(grid::mask)
     }
 
     private fun setMask(cell: SquareCell, masked: Boolean) {
+        hasLabyrinth = false
         if (masked) grid.mask(cell) else grid.unmask(cell)
-        distanceSource = null
-        distance = null
-        currentDistance = 0
     }
 
-    private fun handleSomething(cell: SquareCell) {
-        distanceSource = cell
+    private fun handleSomething() {
+        hasLabyrinth = true
         grid.resetLinks()
-        AldousBroder(grid).forEach {}
-        distance = Distance(cell)
+        AldousBroder.on(grid)
     }
 
     private fun getCellNeighborhood(cell: SquareCell): List<SquareCell> {
@@ -84,42 +77,33 @@ class MaskedSquareGridScene(private val grid: MaskedSquareGrid) : Scene() {
             return
         }
 
-        var neighborhood = getCellNeighborhood(hoveredCell)
         if (!isLeftHeld && Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+            val neighborhood = getCellNeighborhood(hoveredCell)
             isLeftHeld = true
             neighborhood.forEach { setMask(it, false) }
-        }
-        if (!isRightHeld && Gdx.input.isButtonPressed(Input.Buttons.RIGHT)) {
+        } else if (!isRightHeld && Gdx.input.isButtonPressed(Input.Buttons.RIGHT)) {
+            val neighborhood = getCellNeighborhood(hoveredCell)
             isRightHeld = true
             neighborhood.forEach { setMask(it, true) }
         }
 
-        if (!isForwardHeld && Gdx.input.isButtonPressed(Input.Buttons.FORWARD)) {
-            isForwardHeld = true
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.FORWARD) || Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
             range++
         }
-        isForwardHeld = Gdx.input.isButtonPressed(Input.Buttons.FORWARD)
 
-        if (!isBackHeld && Gdx.input.isButtonPressed(Input.Buttons.BACK)) {
-            isBackHeld = true
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.BACK) || Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
             range = maxOf(0, range - 1)
         }
-        isBackHeld = Gdx.input.isButtonPressed(Input.Buttons.BACK)
 
-        if (!isSpaceHeld && Gdx.input.isKeyPressed(Input.Keys.SPACE) && !grid.isMasked(
-                hoveredCell
-            )
-        ) {
-            isSpaceHeld = true
-            handleSomething(hoveredCell)
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            handleSomething()
         }
-        isSpaceHeld = Gdx.input.isKeyPressed(Input.Keys.SPACE)
     }
 
     override fun draw() {
-        ScreenUtils.clear(1f, 1f, 1f, 1f)
-        var neighborHood = prevHoveredCell?.let(::getCellNeighborhood) ?: emptyList()
-        if (distance == null) {
+        ScreenUtils.clear(Color.WHITE)
+        val neighborHood = prevHoveredCell?.let(::getCellNeighborhood) ?: emptyList()
+        if (!hasLabyrinth) {
             grid.allCells().forEach {
                 if (grid.isMasked(it)) {
                     drawer.fill(it, Color.GRAY)
@@ -129,23 +113,10 @@ class MaskedSquareGridScene(private val grid: MaskedSquareGrid) : Scene() {
             return
         }
 
-        val gradientSampler = gradient.sampler(0f, distance!!.max.toFloat())
-        grid.cells().forEach {
-            val cellDist = distance!![it]
-            var color = if (cellDist <= currentDistance) gradientSampler.sample(cellDist.toFloat()) else Color.WHITE
-            if (it.row == prevHoveredCell?.row && it.column == prevHoveredCell?.column) {
-                // lighten color if hovered
-                color = Color(color.r * 1.5f, color.g * 1.5f, color.b * 1.5f, color.a)
-            }
-            drawer.fill(it, color)
-        }
-        currentDistance += 5
-
         if (prevHoveredCell != null) {
             drawer.fill(prevHoveredCell!!, Color.CORAL)
         }
-        shapeRenderer.color = Color.BLACK
-        grid.cells().forEach(drawer::drawAllBorders)
+        drawer.drawUnlinkedBorders(Color.GRAY)
         prevHoveredCell?.let { drawer.drawAllBorders(it, Color.RED) }
     }
 }
