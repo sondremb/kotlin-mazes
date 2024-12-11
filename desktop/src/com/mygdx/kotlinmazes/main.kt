5683f86620e056a76486944e5ac903668e94607a
sondremb <sondremartin@hotmail.com>
2024-12-11 19:31:28 +0100

    Ta viewport tilbake

--- desktop/src/com/mygdx/kotlinmazes/main.kt
+++ desktop/src/com/mygdx/kotlinmazes/main.kt
@@ -7,7 +7,7 @@ import com.mygdx.kotlinmazes.scenes.SquareGridDistance
 import ktx.math.vec2
 
 object EngineConfig {
-    const val VIEWPORT_WIDTH = 1080f
+    const val VIEWPORT_WIDTH = 1920f
     const val VIEWPORT_HEIGHT = 1080f
     val VIEWPORT_CENTER = vec2(VIEWPORT_WIDTH / 2, VIEWPORT_HEIGHT / 2)
     val VIEWPORT_RECT = Rectangle(0f, 0f, VIEWPORT_WIDTH, VIEWPORT_HEIGHT)
